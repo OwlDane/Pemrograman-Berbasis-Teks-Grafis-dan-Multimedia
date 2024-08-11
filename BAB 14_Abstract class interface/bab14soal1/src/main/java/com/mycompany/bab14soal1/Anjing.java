1c1db228b3295d24d252bbe5f2b3c32bdaaaf229
zidan-e <145408875+OwlDane@users.noreply.github.com>
2024-08-11 16:46:13 +0700

    Add files via upload

---
+++ BAB 14_Abstract class interface/bab14soal1/src/main/java/com/mycompany/bab14soal1/Anjing.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.bab14soal1;
+
+/**
+ *
+ * @author HP
+ */
+public class Anjing extends abstrakhewan {
+
+    @Override
+    public void suara() {
+         System.out.println("Anjing bersuara: Guk-guk");
+    }
+}
