f41687324a96d8605dfc628bd506fa611957f866
zidan-e <145408875+OwlDane@users.noreply.github.com>
2024-08-11 13:57:46 +0700

    Add files via upload

---
+++ BAB 13_Polymorphism/bab13soal2/src/main/java/com/mycompany/bab13soal2/Bab13soal2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab13soal2;
+
+/**
+ *
+ * @author HP
+ */
+public class Bab13soal2 {
+
+    public static void main(String[] args) {
+      SegitigaCalculator objek =  new SegitigaCalculator();
+      
+      double luas1 = objek.hitungLuas(16, 17);
+      System.out.print("Luas segitiga dengan alas 16 dan tinggi 17 adalah : " + luas1);
+      
+    }
+}
